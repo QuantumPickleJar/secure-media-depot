691bc0af500d2ff6b9cd96202af5986882d19193
Vincent Morrill <morriv92@uwosh.edu>
2024-09-15 12:17:26 -0500

    chore: Refactor userController.js for improved code structure and error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,10 +10,14 @@ const db = require('../database/db');
 exports.registerUser = async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  db.run(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, hashedPassword], (err) => {
-    if (err) {
-      return res.status(400).send('Username already exists.');
-    }
+
+  db.run(
+    `INSERT INTO users (username, password) VALUES (?, ?)`, 
+    [username, hashedPassword], 
+    (err) => {
+      if (err) {
+        return res.status(400).send('Username already exists.');
+      }
     res.status(201).send('User registered successfully');
   });
 };
@@ -22,10 +26,7 @@ exports.getUserProfile = (req, res) => {
   const username = req.user.username;
 
   db.get(`SELECT username FROM users WHERE username = ?`, [username], (err, user) => {
-    if (err) {
-      return res.status(500).send('Server error');
-    }
-    if (!user) {
+    if (err || !user) {
       return res.status(404).send('User not found');
     }
     res.json({ username: user.username });
