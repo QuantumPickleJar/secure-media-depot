7773d618a9c6441238c2b3667bc9469179bfc69d
Vincent Morrill <vmorrilll@gmail.com>
2024-10-07 20:13:40 -0500

    refactor: Update fileRoutes.js to use relative paths

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const router = express.Router();
-const authenticateJWT = require('../auth/authMiddleware');
+const { authenticateJWT } = require('../auth/authMiddleware');
 const authorizeAdmin = require('../auth/authMiddleware').authorizeAdmin;
 const fileController = require('../controllers/fileController');
 const upload = require('../config/multer');
@@ -31,7 +31,7 @@ router.post('/upload', authenticateJWT, upload.single('file'), fileController.up
  * @param {callback} middleware - Express middleware
  * @param {callback} controller - Controller function for listing all files
  */
-router.get('/files', authenticateJWT, fileController.listFiles);
+router.get('/', authenticateJWT, fileController.listFiles);
 
 /**
  * Route for getting a file by ID.
@@ -42,8 +42,8 @@ router.get('/files', authenticateJWT, fileController.listFiles);
  * @param {callback} middleware - Express middleware
  * @param {callback} controller - Controller function for getting a file by ID
  */
-router.get('/files/:id', authenticateJWT, fileController.getFileById);
+router.get('/:id', authenticateJWT, fileController.getFileById);
 
-router.get('/files/search', authenticateJWT, fileController.searchFiles)
+router.get('/search', authenticateJWT, fileController.searchFiles)
 
 module.exports = router;
