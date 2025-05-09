b8f4cd0fed3931fc87b4c63207595cac2c40b23a
QuantumPickleJar <vince.codefactory@outlook.com>
2025-05-09 18:27:49 -0500

    fix: correct casing of 'String' in Video model column definitions

--- python/models/video_model.py
+++ python/models/video_model.py
@@ -7,9 +7,9 @@ class Video(db.Model):
     __tablename__ = 'videos'
     id = db.Column(db.Integer, primary_key=True)
     # unique key to identify objects
-    key = db.Column(db.string(255), unique=True, index=True, nullable=False)
+    key = db.Column(db.String(255), unique=True, index=True, nullable=False)
     # title should be searchable
-    title = db.Column(db.string(255), index = True, nullable=False)
+    title = db.Column(db.String(255), index = True, nullable=False)
     # byte-size on disk
     size_bytes = db.Column(db.Integer, nullable=False)
     # mime type (video/mp4, etc.)
